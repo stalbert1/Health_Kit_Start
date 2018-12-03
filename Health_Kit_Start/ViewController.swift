410e824a26ff61c6eb763a9eec40cd90e2ca0c08
Shane Talbert <stalbert1@tampabay.rr.com>
2018-12-03 11:07:53 -0500

    Initial Commit

---
+++ Health_Kit_Start/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Health_Kit_Start
+//
+//  Created by Shane Talbert on 12/3/18.
+//  Copyright © 2018 Shane Talbert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
